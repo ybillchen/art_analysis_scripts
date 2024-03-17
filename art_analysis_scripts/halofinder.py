c3d0dedd31571e6aa3a1d776ccfaa751fe80ec2b
Bill Chen (Mac) <ybchen@umich.edu>
2024-03-17 15:06:49 -0400

    updated

--- art_analysis_scripts/halofinder.py
+++ art_analysis_scripts/halofinder.py
@@ -13,23 +13,23 @@ yt.enable_parallelism()
 from yt_astro_analysis.halo_analysis import HaloCatalog
 
 def rockstar_halofinder(restart=0, base="", num_readers=8, 
-	particle_type="N-BODY"):
+    particle_type="N-BODY"):
 
-	ts = yt.load(os.path.join(base, "out/snap_a*.art"))
+    ts = yt.load(os.path.join(base, "out/snap_a*.art"))
 
-	for ds in ts:
-		# https://yt-astro-analysis.readthedocs.io/en/latest/Installation.html
-	    ds.parameters["format_revision"] = 2
+    for ds in ts:
+        # https://yt-astro-analysis.readthedocs.io/en/latest/Installation.html
+        ds.parameters["format_revision"] = 2
 
-	hc = HaloCatalog(data_ds=ts, finder_method="rockstar", 
-		finder_kwargs={
-			"num_readers": num_readers,
-			"particle_type": particle_type,
-			"outbase": os.path.join(base, "rockstar_halos"),
-			"restart": bool(restart)
-			})
+    hc = HaloCatalog(data_ds=ts, finder_method="rockstar", 
+        finder_kwargs={
+            "num_readers": num_readers,
+            "particle_type": particle_type,
+            "outbase": os.path.join(base, "rockstar_halos"),
+            "restart": bool(restart)
+            })
 
-	hc.create()
+    hc.create()
 
 if __name__ == "__main__":
 
@@ -38,4 +38,4 @@ if __name__ == "__main__":
     else:
         restart = sys.argv[1]
 
-	rockstar_halofinder(restart=restart)
+    rockstar_halofinder(restart=restart)
