d61e1fccdcda756d01a6211b9153a87a57edaca2
Bill Chen (Mac) <ybchen@umich.edu>
2024-03-17 15:02:13 -0400

    updated

--- art_analysis_scripts/halofinder.py
+++ art_analysis_scripts/halofinder.py
@@ -4,14 +4,18 @@ Copyright (c) 2024 Yingtian Chen
 All rights reserved.
 """
 
+import os
+import sys
+
 import yt
 
 yt.enable_parallelism()
 from yt_astro_analysis.halo_analysis import HaloCatalog
 
-if __name__ == "__main__":
+def rockstar_halofinder(restart=0, base="", num_readers=1, 
+	particle_type="N-BODY"):
 
-	ts = yt.load("out/snap_a*.art")
+	ts = yt.load(os.path.join(base, "out/snap_a*.art"))
 
 	for ds in ts:
 		# https://yt-astro-analysis.readthedocs.io/en/latest/Installation.html
@@ -19,9 +23,19 @@ if __name__ == "__main__":
 
 	hc = HaloCatalog(data_ds=ts, finder_method="rockstar", 
 		finder_kwargs={
-			"num_readers": 1,
-			"particle_type": "N-BODY",
-			"restart": False
+			"num_readers": num_readers,
+			"particle_type": particle_type,
+			"outbase": os.path.join(base, "rockstar_halos"),
+			"restart": bool(restart)
 			})
 
 	hc.create()
+
+if __name__ == "__main__":
+
+    if len(sys.argv) != 2:
+        restart = 0
+    else:
+        restart = sys.argv[1]
+
+	rockstar_halofinder(restart=restart)
