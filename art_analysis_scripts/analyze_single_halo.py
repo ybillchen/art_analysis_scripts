5b218feacde25ef6a85279583b5bcba532920f95
Bill Chen (Mac) <ybchen@umich.edu>
2024-03-21 13:31:12 -0400

    updated

--- art_analysis_scripts/analyze_single_halo.py
+++ art_analysis_scripts/analyze_single_halo.py
@@ -17,18 +17,20 @@ import prj_plotter as pp
 
 if __name__ == "__main__":
     # assume that this script is executed in run/
-    snap_last = yt.load("out/snap_a1.0017.art")
+    # snap_last = yt.load("out/snap_a1.0017.art")
     snap_last = yt.load("out/snap_a0.2510.art")
     a = ytree.load("rockstar_halos/trees/arbor/arbor.h5")
     trees = list(a[:])
 
-    radius = 1000.0  # in kpc
+    radius = 250.0  # in kpc
     box = [-radius, -radius, 2*radius, 2*radius]
 
     for tree in ytree.parallel_trees(trees, save_every=False):
         if tree["mass"] > a.quan(4.8e12, "Msun") and \
             tree["mass"] < a.quan(5.2e12, "Msun"):
             root = tree.find_root()
+            hid = root["uid"]
+
             prog = list(tree["prog"])
             redshift_prog = np.array(tree["prog", "redshift"])
             idx = np.where((redshift_prog>2.95)&(redshift_prog<3.05))
@@ -43,21 +45,19 @@ if __name__ == "__main__":
             #     continue
             scale_a = 1.0/(1.0+redshift)
 
-            hid = root["uid"]
-
             # in ytree, units are always comoving 
             # need to manually convert to physical
-            center = root["position"] * scale_a
-            rvir = root["virial_radius"] * scale_a
+            center = root["position"].to("kpc") * scale_a
+            rvir = root["virial_radius"].to("kpc") * scale_a
 
             sp = snap_last.sphere(center, (radius, "kpc"))
 
             fig, ax0 = plt.subplots()
 
-            x = sp[("N-BODY", "POSITION_X")] - center[0]
-            y = sp[("N-BODY", "POSITION_Y")] - center[1]
+            x = sp[("N-BODY", "POSITION_X")].to("kpc") - center[0]
+            y = sp[("N-BODY", "POSITION_Y")].to("kpc") - center[1]
 
-            pp.prj(ax0, x.to("kpc"), y.to("kpc"), 
+            pp.prj(ax0, x, y, 
                 box=box, vmin=-4, vmax=1, log=True, capacity=64, 
                 max_level=10, cmap=plt.cm.magma)
 
