92e825b7098f2558312f9e06303601a0e777f653
Bill Chen (Mac) <ybchen@umich.edu>
2024-03-23 14:03:48 -0400

    updated

---
+++ art_analysis_scripts/select_zoom_particles.py
@@ -0,0 +1,63 @@
+"""
+BSD 3-Clause License
+Copyright (c) 2024 Yingtian Chen
+All rights reserved.
+"""
+
+import numpy as np
+import matplotlib
+matplotlib.use("agg")
+import matplotlib.pyplot as plt
+
+import yt
+yt.enable_parallelism()
+import ytree
+
+import prj_plotter as pp
+
+def zoom_particles_from_z3(hid, factor=4):
+    snap = yt.load("out/snap_a0.2510.art")
+    snap_first = yt.load("out/snap_a0.010.art").all_data()
+    a = ytree.load("rockstar_halos/trees/arbor/arbor.h5")
+    trees = list(a[:])
+
+    tree = trees[np.array(trees["uid"], dtype=int)==hid]
+
+    prog = list(tree["prog"])
+    redshift_prog = np.array(tree["prog", "redshift"])
+    idx = np.where((redshift_prog>2.95)&(redshift_prog<3.05))
+    assert len(idx[0]) == 1
+    node = prog[idx[0][0]]
+
+    redshift = node['redshift']
+    scale_a = 1.0/(1.0+redshift)
+
+    # in ytree, units are always comoving 
+    # need to manually convert to physical
+    center = node["position"].to("kpc") * scale_a
+    rvir = node["virial_radius"].to("kpc") * scale_a
+
+    sp = snap.sphere(center, rvir * factor)
+    pids = sp[("N-BODY", "PID")]
+
+    pids_first = snap_first[("N-BODY", "PID")]
+
+    xy, x_ind, y_ind = np.intersect1d(pids, pids_first, 
+        assume_unique=True, return_indices=True)
+    assert len(xy) == len(pids)
+
+    x = pids_first[("N-BODY", "POSITION_X")][y_ind].to("unitary")
+    y = pids_first[("N-BODY", "POSITION_Y")][y_ind].to("unitary")
+    z = pids_first[("N-BODY", "POSITION_Z")][y_ind].to("unitary")
+
+    print("x range: %.3f - %.3f"%(np.min(x), np.max(x)))
+    print("y range: %.3f - %.3f"%(np.min(y), np.max(y)))
+    print("z range: %.3f - %.3f"%(np.min(z), np.max(z)))
+
+    out = np.column_stack([x,y,z])
+    np.savetxt("analysis/2e12/zoom_particles_from_z3_%d.txt"%hid, out, 
+        fmt="%.6f %.6f %.6f")
+
+if __name__ == "__main__":
+    hid = 1139710
+    zoom_particles_from_z3(1139710)
