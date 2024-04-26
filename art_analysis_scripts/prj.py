677fb19818b130a71240bab729c29436b0747861
Bill Chen (Mac) <ybchen@umich.edu>
2024-04-26 14:30:51 -0400

    updated

--- art_analysis_scripts/prj.py
+++ art_analysis_scripts/prj.py
@@ -75,9 +75,9 @@ if __name__ == "__main__":
 
     fig, [ax0, ax1] = plt.subplots(1,2)
 
-    mesh, region = prj(ds, [x0, y0, z0], 1.0, level=12, prj_x="x", prj_y="y", field="density", unit="Msun/pc**3")
+    mesh, region = prj(ds, [x0, y0, z0], 2.0, level=12, prj_x="x", prj_y="y", field="density", unit="Msun/pc**3")
     ax0.imshow(np.log10(mesh.T), origin="lower", extent=[region[0],region[3],region[1],region[4]])
-    mesh, region = prj(ds, [x0, y0, z0], 1.0, level=12, prj_x="x", prj_y="z", field="density", unit="Msun/pc**3")
+    mesh, region = prj(ds, [x0, y0, z0], 2.0, level=12, prj_x="x", prj_y="z", field="density", unit="Msun/pc**3")
     ax1.imshow(np.log10(mesh.T), origin="lower", extent=[region[0],region[3],region[2],region[5]])
 
     ax0.set_xlabel(r"x (code_length)")
