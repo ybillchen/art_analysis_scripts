ab61b291a06c4453f954d530e82b0bd10c53e73e
Bill Chen (Mac) <ybchen@umich.edu>
2024-05-01 11:43:26 -0400

    updated

--- art_analysis_scripts/timing.py
+++ art_analysis_scripts/timing.py
@@ -9,14 +9,11 @@ import datetime
 
 import numpy as np
 
-def check_timing(path):
-    f = open(path)
+def check_timing(path_to_log):
+    f = open(path_to_log+"timing.000.log")
     data = f.read().split("\n")
     f.close()
-
     step = []
-    t = []
-    a = []
     total_run_time = []
     for line in data:
         x = line.split(" ")
@@ -24,23 +21,34 @@ def check_timing(path):
             continue
 
         step.append(int(x[0]))
-        t.append(float(x[1]))
-        a.append(float(x[2]))
         total_run_time.append(float(x[3]))
-
     step = np.array(step)
-    t = np.array(t)
-    a = np.array(a)
     total_run_time = np.array(total_run_time)
 
-    t_per_runtime = t / total_run_time # yr per s
+    f = open(path_to_log+"times.log")
+    data = f.read().split("\n")
+    f.close()
+    step = []
+    dt = []
+    for line in data:
+        x = line.split(" ")
+        if x[0] == "#" or x[0] == "":
+            continue
+
+        step.append(int(x[0]))
+        dt.append(float(x[2]))
+
+    step = np.array(step)
+    dt = np.array(dt)
+
+    t_per_runtime = dt / total_run_time # yr per s
 
     print(t_per_runtime)
 
 if __name__ == '__main__':
     if len(sys.argv) == 1:
-        path = "log/timing.000.log"
+        path_to_log = "log/"
     elif len(sys.argv) == 2:
-        path = sys.argv[1]
+        path_to_log = sys.argv[1]
 
     check_timing(path)
