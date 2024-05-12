649fb317acfa3d6b4c14484c599f432361783a63
Bill Chen (Mac) <ybchen@umich.edu>
2024-05-12 14:18:01 +0800

    updated

---
+++ art_analysis_scripts/pack_files.py
@@ -0,0 +1,58 @@
+import os
+import tarfile
+import glob
+from multiprocessing import Pool
+import argparse
+
+# Base directory path
+base_dir = os.environ['SCRATCH']
+
+def archive_files(args):
+    identifier, files, check_exists = args
+    target_dir = os.path.dirname(files[0])
+    tar_filename = os.path.join(target_dir, f'snap_a{identifier}.tar')
+    
+    # Check if tar file exists and skip if option is set
+    if check_exists and os.path.exists(tar_filename):
+        print(f'{tar_filename} already exists. Skipping...')
+        return
+    
+    with tarfile.open(tar_filename, 'w') as tar:
+        for file in files:
+            tar.add(file, arcname=os.path.basename(file))
+    print(f'Created {tar_filename}')
+
+def find_files():
+    # Dictionary to hold the grouping of files by identifier [A]
+    file_dict = {}
+    
+    # Walk through the directory structure
+    for root, dirs, files in os.walk(base_dir):
+        for file in files:
+            if 'out' in root.split(os.sep) and file.startswith('snap_a'):
+                filename_parts = file.split('.')
+                identifier = '.'.join(filename_parts[:-1])  # Join all parts except the extension
+                full_path = os.path.join(root, file)
+                if identifier in file_dict:
+                    file_dict[identifier].append(full_path)
+                else:
+                    file_dict[identifier] = [full_path]
+    return file_dict.items()
+
+def main(args):
+    file_groups = find_files()
+    
+    # Prepare arguments for each process
+    process_args = [(identifier, files, args.check_exists) for identifier, files in file_groups]
+    
+    # Using a pool of workers to process the archiving in parallel
+    with Pool(processes=args.max_processes) as pool:
+        pool.map(archive_files, process_args)
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Archive files based on identifiers')
+    parser.add_argument('--max-processes', type=int, default=os.cpu_count(), help='Maximum number of parallel processes')
+    parser.add_argument('--check-exists', action='store_true', help='Check if tar file exists and skip if so')
+    args = parser.parse_args()
+    
+    main(args)
