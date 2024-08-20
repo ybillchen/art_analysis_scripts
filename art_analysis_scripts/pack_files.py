3bc84543dc2d9bdab8e2aafb0a3e0337308d1958
Bill Chen (Mac) <ybchen@umich.edu>
2024-08-20 15:42:26 -0400

    updated

--- art_analysis_scripts/pack_files.py
+++ art_analysis_scripts/pack_files.py
@@ -1,34 +1,26 @@
 import os
 import tarfile
 import glob
-from multiprocessing import Pool, Lock
+from multiprocessing import Pool
 import argparse
 
 # Base directory path
 base_dir = os.environ['SCRATCH']
 
-lock = None
-
-def init(l):
-    global lock
-    lock = l
-
 def archive_files(args):
-    i, identifier, files, check_exists, ntot, lock = args
+    i, identifier, files, check_exists, ntot = args
     target_dir = os.path.dirname(files[0])
     tar_filename = os.path.join(target_dir, f'{identifier}.tar')
     
     # Check if tar file exists and skip if option is set
     if check_exists and os.path.exists(tar_filename):
-        with lock:
-            print(f'{i}/{ntot}: {tar_filename} already exists. Skipped.')
+        print(f'{i}/{ntot}: {tar_filename} already exists. Skipped.\n', end='')
         return
     
     with tarfile.open(tar_filename, 'w') as tar:
         for file in files:
             tar.add(file, arcname=os.path.basename(file))
-    with lock:
-        print(f'{i}/{ntot}: {tar_filename} created.')
+    print(f'{i}/{ntot}: {tar_filename} created.')
 
 def find_files():
     file_dict = {}
@@ -51,12 +43,11 @@ if __name__ == '__main__':
     parser.add_argument('--check-exists', action='store_true', help='Check if tar file exists and skip if so')
     args = parser.parse_args()
     
-    lock = Lock()
     file_groups = find_files()
-    process_args = [(i, identifier, files, args.check_exists, len(file_groups), lock) for 
+    process_args = [(i, identifier, files, args.check_exists, len(file_groups)) for 
         i, (identifier, files) in enumerate(file_groups)]
     
     print('Number of tar files to create: %d'%len(file_groups))
     print('Number of processes: %d'%args.max_processes)
-    with Pool(processes=args.max_processes, initializer=init, initargs=(lock,)) as pool:
+    with Pool(processes=args.max_processes) as pool:
         pool.map(archive_files, process_args)
