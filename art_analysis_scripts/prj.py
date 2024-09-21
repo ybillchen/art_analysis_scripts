b9ccb3fea492e76db31c12e89724c492573b3bba
Bill Chen (Mac) <ybchen@umich.edu>
2024-09-21 15:52:19 -0400

    updated

--- art_analysis_scripts/prj.py
+++ art_analysis_scripts/prj.py
@@ -99,7 +99,7 @@ if __name__ == "__main__":
     size = 0.125
 
     unit = "kpc"
-    unit_convert = ds.units.code_length.to(unit).value
+    unit_convert, unit_offset = ds.units.code_length.get_conversion_factor(unit)
 
     fig, [ax0, ax1] = plt.subplots(1,2)
 
