00d044da44160292b27534a36960a988ff2c3b66
Bill Chen (Mac) <ybchen@umich.edu>
2024-10-12 12:20:40 -0400

    updated

--- art_analysis_scripts/skirt_interface.py
+++ art_analysis_scripts/skirt_interface.py
@@ -17,10 +17,10 @@ def art2skirt(ds, region, center, savenamebase):
     ys = region[("STAR", "POSITION_Y")].to_value("pc") - center[1].to_value("pc")
     zs = region[("STAR", "POSITION_Z")].to_value("pc") - center[2].to_value("pc")
     es = np.zeros_like(xs)
-    print("loading star velocities")
-    vxs = region[("STAR", "VELOCITY_X")].to_value("km/s")
-    vys = region[("STAR", "VELOCITY_Y")].to_value("km/s")
-    vzs = region[("STAR", "VELOCITY_Z")].to_value("km/s")
+    # print("loading star velocities")
+    # vxs = region[("STAR", "VELOCITY_X")].to_value("km/s")
+    # vys = region[("STAR", "VELOCITY_Y")].to_value("km/s")
+    # vzs = region[("STAR", "VELOCITY_Z")].to_value("km/s")
     print("loading star masses, metallicities, and ages")
     ms = region[("STAR", "INITIAL_MASS")].to_value("Msun")
     Zs = (region[('STAR', 'METALLICITY_SNII')]+region[('STAR', 'METALLICITY_SNIa')]+region[('STAR', 'METALLICITY_AGB')]).to_value("1")
@@ -31,16 +31,13 @@ def art2skirt(ds, region, center, savenamebase):
         "%s.txt: import file for particle source\n"%savenames + 
         "Column 1: position x (pc)\n"
         "Column 2: position y (pc)\n"
-        "Column 2: position z (pc)\n"
+        "Column 3: position z (pc)\n"
         "Column 4: smoothing length (pc)\n"
-        "Column 5: velocity vx (km/s)\n"
-        "Column 6: velocity vy (km/s)\n"
-        "Column 7: velocity vz (km/s)\n"
-        "Column 8: mass (Msun)\n"
-        "Column 9: metallicity (1)\n"
-        "Column 10: age (Gyr)\n"
+        "Column 5: mass (Msun)\n"
+        "Column 6: metallicity (1)\n"
+        "Column 7: age (Gyr)\n"
         )
-    outs = np.column_stack([xs, ys, zs, es, vxs, vys, vzs, ms, Zs, ts])
+    outs = np.column_stack([xs, ys, zs, es, ms, Zs, ts])
     np.savetxt(savenames, outs, header=headers, fmt="%.6e")
 
     print("loading gas positions")
@@ -58,10 +55,10 @@ def art2skirt(ds, region, center, savenamebase):
     mg = region[("gas", "mass")].to_value("Msun")
     Zg = region[("gas", "metallicity")].to_value("1")
     Tg = region[("gas", "temperature")].to_value("K")
-    print("loading gas velocities")
-    vxg = region[("gas", "velocity_x")].to_value("km/s")
-    vyg = region[("gas", "velocity_y")].to_value("km/s")
-    vzg = region[("gas", "velocity_z")].to_value("km/s")
+    # print("loading gas velocities")
+    # vxg = region[("gas", "velocity_x")].to_value("km/s")
+    # vyg = region[("gas", "velocity_y")].to_value("km/s")
+    # vzg = region[("gas", "velocity_z")].to_value("km/s")
 
     savenameg = savenamebase + "_gas.txt"
     headerg = (
@@ -72,14 +69,11 @@ def art2skirt(ds, region, center, savenamebase):
         "column 4: xmax (pc)\n"
         "column 5: ymax (pc)\n"
         "column 6: zmax (pc)\n"
-        "Column 7: gas mass (Msun/pc3)\n"
+        "Column 7: gas mass (Msun)\n"
         "Column 8: metallicity (1)\n"
         "Column 9: temperature (K)\n"
-        "Column 10: velocity vx (km/s)\n"
-        "Column 11: velocity vy (km/s)\n"
-        "Column 12: velocity vz (km/s)\n"
         )
-    outg = np.column_stack([x0g, y0g, z0g, x1g, y1g, z1g, mg, Zg, Tg, vxg, vyg, vzg])
+    outg = np.column_stack([x0g, y0g, z0g, x1g, y1g, z1g, mg, Zg, Tg])
     np.savetxt(savenameg, outg, header=headerg, fmt="%.6e")
 
 
@@ -97,11 +91,11 @@ if __name__ == "__main__":
     center = tree["position"].to("kpc") * scale_a
 
     radius = 10.0 * ds.units.kpc # in kpc
-    
+
     # region = [
     #     center[0]-radius, center[1]-radius, center[2]-radius, 
     #     center[0]+radius, center[1]+radius, center[2]+radius]
     # region = ds.box(region[:3], region[3:])
 
     region = ds.sphere(center, radius)
-    art2skirt(ds, region, center, "analysis/skirt_a%.4f"%scale_a)
+    art2skirt(ds, region, center, "analysis/skirt/skirt_a%.4f"%scale_a)
