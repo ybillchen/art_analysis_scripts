071a0847907db4e5947955e69c037dc3d1b19e47
Bill Chen (Mac) <ybchen@umich.edu>
2024-10-31 14:09:01 -0400

    updated

--- art_analysis_scripts/global_star_property.py
+++ art_analysis_scripts/global_star_property.py
@@ -14,52 +14,53 @@ from age_spreads import time_units, duration, ave_time, age_spread
 
 def load_ds(basepath, a_target):
     filename = os.path.join(basepath, "run/out/snap_a%.4f.art"%a_target)
-    filename = filename if os.path.isfile(filename) else os.path.join(basepath, "out/snap_a%.4f.art"%a_target)
-    return yt.load(filename)
+    is_under_run = os.path.isfile(filename)
+    filename = filename if is_under_run else os.path.join(basepath, "out/snap_a%.4f.art"%a_target)
+    return yt.load(filename), is_under_run
 
 def log_init_mass(region):
     return np.log10(region[("STAR", "INITIAL_MASS")].to_value("Msun"))
 
 def save_logmi(basepath, a_target):
-    ds = load_ds(basepath, a_target)
+    ds, is_under_run = load_ds(basepath, a_target)
     snap = ds.all_data()
 
     logmi = logmi(snap)
-    np.savetxt(os.path.join(basepath, "run/analysis/logmi.txt"), logmi)
+    np.savetxt(os.path.join(basepath, "run/" if is_under_run else "", "analysis/logmi.txt"), logmi)
 
 def save_tave(basepath, a_target):
-    ds = load_ds(basepath, a_target)
+    ds, is_under_run = load_ds(basepath, a_target)
     snap = ds.all_data()
 
     tave = ave_time(snap)
-    np.savetxt(os.path.join(basepath, "run/analysis/tave.txt"), tave)
+    np.savetxt(os.path.join(basepath, "run/" if is_under_run else "", "analysis/tave.txt"), tave)
 
 def save_tdur(basepath, a_target):
-    ds = load_ds(basepath, a_target)
+    ds, is_under_run = load_ds(basepath, a_target)
     snap = ds.all_data()
 
     tave = duration(snap)
-    np.savetxt(os.path.join(basepath, "run/analysis/tdur.txt"), tdur)
+    np.savetxt(os.path.join(basepath, "run/" if is_under_run else "", "analysis/tdur.txt"), tdur)
 
 def save_tspread(basepath, a_target):
-    ds = load_ds(basepath, a_target)
+    ds, is_under_run = load_ds(basepath, a_target)
     snap = ds.all_data()
 
     tspread = age_spread(snap)
-    np.savetxt(os.path.join(basepath, "run/analysis/tspread.txt"), tspread)
+    np.savetxt(os.path.join(basepath, "run/" if is_under_run else "", "analysis/tspread.txt"), tspread)
 
 def save_all(basepath, a_target):
-    ds = load_ds(basepath, a_target)
+    ds, is_under_run = load_ds(basepath, a_target)
     snap = ds.all_data()
 
     logmi = log_init_mass(snap)
-    np.savetxt(os.path.join(basepath, "run/analysis/logmi.txt"), logmi)
+    np.savetxt(os.path.join(basepath, "run/" if is_under_run else "", "analysis/logmi.txt"), logmi)
     tave = ave_time(snap)
-    np.savetxt(os.path.join(basepath, "run/analysis/tave.txt"), tave)
+    np.savetxt(os.path.join(basepath, "run/" if is_under_run else "", "analysis/tave.txt"), tave)
     tave = duration(snap)
-    np.savetxt(os.path.join(basepath, "run/analysis/tdur.txt"), tdur)
+    np.savetxt(os.path.join(basepath, "run/" if is_under_run else "", "analysis/tdur.txt"), tdur)
     tspread = age_spread(snap)
-    np.savetxt(os.path.join(basepath, "run/analysis/tspread.txt"), tspread)
+    np.savetxt(os.path.join(basepath, "run/" if is_under_run else "", "analysis/tspread.txt"), tspread)
 
 if __name__ == '__main__':
     if len(sys.argv) == 1:
