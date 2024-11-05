7dc01fe0fc19e8cb51ea02eb84414aefc0d4d3b0
Bill Chen (Mac) <ybchen@umich.edu>
2024-11-05 06:01:52 -0500

    updated

---
+++ art_analysis_scripts/halo_star_property.py
@@ -0,0 +1,81 @@
+"""
+BSD 3-Clause License
+Copyright (c) 2024 Yingtian Chen
+All rights reserved.
+"""
+
+import os
+import sys
+
+import numpy as np
+
+import yt
+yt.enable_parallelism()
+
+from age_spreads import time_units, duration, ave_time, age_spread
+
+# scale(0) id(1) desc_scale(2) desc_id(3) num_prog(4) pid(5) upid(6) desc_pid(7) 
+# phantom(8) sam_Mvir(9) Mvir(10) Rvir(11) rs(12) vrms(13) mmp?(14) scale_of_last_MM(15) 
+# vmax(16) x(17) y(18) z(19) vx(20) vy(21) vz(22) Jx(23) Jy(24) Jz(25) Spin(26) 
+# Breadth_first_ID(27) Depth_first_ID(28) Tree_root_ID(29) Orig_halo_ID(30) Snap_idx(31) 
+# Next_coprogenitor_depthfirst_ID(32) Last_progenitor_depthfirst_ID(33) 
+# Last_mainleaf_depthfirst_ID(34) Tidal_Force(35) Tidal_ID(36)
+
+def find_most_massive_halo(tree, a_target):
+    snap = tree[:,31]
+
+    if a_target is None or a_target < 0 or a_target > 1.1:
+        mask_now = snap == np.max(snap)
+    else:
+        mask_now = np.abs(tree[:,0]-a_target)<5e-5
+
+    idx_mmax = np.argmax(newtree[mask_now,10])
+    mainleaf_id = newtree[mask_now,34][idx_mmax]
+
+    mask_mb = tree[:,34] == mainleaf_id
+    mb = tree[mask_mb]
+    mb = mb[np.argsort(mb[:,0])]
+
+    return mb
+
+def sfh(ts, branch, agecut=50):
+    storage = {}
+
+    for store, ds in ts.piter(storage=storage):
+        scale = 1 / (ds.current_redshift+1)
+        idx = np.where(np.abs(branch[:,0]-scale)<5e-5)[0]
+        if len(idx) == 0:
+            continue
+        assert len(idx) == 1
+        line = branch[idx[0]]
+        hpos = halo_tree[17:20] * ds.arr(1, "Mpccm/h")
+        sp = ds.sphere(hpos, rvir)
+
+        ms_i = sp[("STAR", "INITIAL_MASS")].to_value("Msun")
+        tform = sp[("STAR", "creation_time")].to_value("Myr")
+        tnow  = ds.current_time.in_units("Myr")
+        tage = tnow - tform
+        mask = tage < agecut
+        ms_cut = np.sum(ms_i[mask])
+
+        store.result = (scale, tnow, ms_cut/agecut)
+
+    return np.array(list(storage.values()))
+
+if __name__ == '__main__':
+    if len(sys.argv) == 1:
+        basepath = ""
+        a_target = None
+    elif len(sys.argv) == 2:
+        basepath = ""
+        a_target = float(sys.argv[1])
+    elif len(sys.argv) == 3:
+        basepath = sys.argv[2]
+        a_target = float(sys.argv[1])
+
+    tree = np.loadtxt(os.path.join(basepath, "run/" if is_under_run else "", "rockstar_halos/trees/tree_0_0_0.dat"))
+    ts = yt.load(os.path.join(basepath, "run/" if is_under_run else "", "out/snap_a*.art"))
+
+    mb = find_most_massive_halo(tree, a_target)
+    mass_history_50 = sfh(ts, mb, agecut=50)
+    print(mass_history_50)
