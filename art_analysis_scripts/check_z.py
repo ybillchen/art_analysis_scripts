826584ad72116479676a19a28e69648dbc855b5d
Bill Chen (Mac) <ybchen@umich.edu>
2025-11-15 17:44:20 -0500

    updated

--- art_analysis_scripts/check_z.py
+++ art_analysis_scripts/check_z.py
@@ -84,5 +84,5 @@ def check_stuck(root_path, root_folders):
 if __name__ == "__main__":
     root_path = "/scratch/08199/tg874988/art_simulations/hydro"
     root_folders = ["mh2e12_km", "mh3e12_km", "mh5e12_km", "mh2e12_p12", "mh3e12_p12", "mh5e12_p12"]
-    root_folders = ["mh5e12_km", "mh5e12_p12"]
+    # root_folders = ["mh5e12_km", "mh5e12_p12"]
     check_stuck(root_path, root_folders)
