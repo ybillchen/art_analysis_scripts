b0c227dc54671892c96284c7df7fb74c97a95e7d
Bill Chen (Mac) <ybchen@umich.edu>
2025-11-21 12:54:31 -0500

    updated

--- art_analysis_scripts/track_tree.py
+++ art_analysis_scripts/track_tree.py
@@ -12,6 +12,7 @@ sys.path.append('.')
 import yt
 import numpy as np
 
+from prj import prj
 from age_spreads import *
 from utils import *
 
@@ -75,12 +76,99 @@ def find_main_mpb(tree):
 
     return mpb_main[mpb_main[:,31].argsort()] # sort by snap number
 
+def make_prj_single(snapshot, filename, basepath):
+
+    ds = yt.load(filename)
+    center = (snapshot[17:20]*ds.units.Mpccm/ds.units.h).to_value('code_length')
+
+    d = ds.all_data()
+
+    x0 = center[0]
+    y0 = center[1]
+    z0 = center[2]
+    size = (10.0*ds.units.kpc).to_value('code_length')
+
+    level = 9
+    factor = 0.6
+
+    unit = 'kpc'
+    unit_convert = (1.0*ds.units.code_length).to_value(unit)
+
+    ruler = 1.0 # in kpc
+    ruler_convert = (ruler*ds.units.kpc).to_value(unit)
+
+    fig, ax0 = plt.subplots(1, 1, figsize=(3,3))
+    axs = [ax0]
+    # fig, axs = plt.subplots(1, 2, figsize=(6,3))
+
+    prjs = ["x", "y", "z"]
+    centers = [x0, y0, z0]
+
+    for ax0, idx_x, idx_y in zip(axs, [0, 0], [1, 2]):
+
+        # gas
+        mesh, region = prj(ds, [x0, y0, z0], 
+            size, level=level, prj_x=prjs[idx_x], prj_y=prjs[idx_y], 
+            field="density", unit="Msun/pc**3", factor=factor)
+        ax0.imshow(
+            mesh.T, origin="lower", norm=LogNorm(vmin=1e-6, vmax=1e-2),
+            extent=[region[idx_x].to_value(unit), region[idx_x+3].to_value(unit),
+                region[idx_y].to_value(unit), region[idx_y+3].to_value(unit)])
+
+        # stars
+        d = ds.box(region[:3], region[3:])
+        ax0.scatter(
+            d["STAR", "POSITION_%s"%prjs[idx_x].upper()].to_value(unit),
+            d["STAR", "POSITION_%s"%prjs[idx_y].upper()].to_value(unit), 
+            fc='w', ec='none', s=d["STAR", "MASS"].to_value("Msun")/5e5, alpha=0.7)
+
+        # ruler
+        ax0.plot(
+            [
+                (centers[idx_x]+0.45*size)*unit_convert-ruler_convert, 
+                (centers[idx_x]+0.45*size)*unit_convert
+            ], [
+                (centers[idx_y]-0.45*size)*unit_convert, 
+                (centers[idx_y]-0.45*size)*unit_convert
+            ], lw=1.5, c="w")
+        ax0.text(
+            (centers[idx_x]+0.45*size)*unit_convert-0.5*ruler_convert, 
+            (centers[idx_y]-0.44*size)*unit_convert, 
+            r"%d kpc"%ruler, ha="center", va="bottom", color="w")
+
+        ax0.set_xlabel(r"%s (%s)"%(prjs[idx_x], unit))
+        ax0.set_ylabel(r"%s (%s)"%(prjs[idx_y], unit))
+        ax0.set_axis_off()
+        ax0.set_aspect("equal")
+        ax0.set_xlim(
+            (centers[idx_x]-0.5*size)*unit_convert, 
+            (centers[idx_x]+0.5*size)*unit_convert)
+        ax0.set_ylim(
+            (centers[idx_y]-0.5*size)*unit_convert, 
+            (centers[idx_y]+0.5*size)*unit_convert)
+
+    # axs[0].text(
+    #     (centers[idx_x]-0.45*size)*unit_convert, 
+    #     (centers[idx_y]+0.45*size)*unit_convert, 
+    #     r"$R_{\rm GMC} = %d$ pc"%10, ha="left", va="top", color="w")
+
+    plt.tight_layout()
+    plt.savefig(
+        filename.replace('out/snap_', 'analysis/prj_').replace('.art', '.png'), 
+        bbox_inches ="tight", pad_inches=0.05, dpi=300
+    )
+    plt.close()
+
+    print('Done a = %.4f'%a)
+
 def make_prj_along_mpb(mpb, filename_list_for_tree, basepath):
     for idx in range(len(mpb)):
         snapshot = mpb[idx]
         currentsnap = int(snapshot[31])
         filename = os.path.join(basepath, filename_list_for_tree[currentsnap])
         print(idx, currentsnap, snapshot[0], snapshot[17:20], filename)
+        if idx == len(mpb) - 1:
+            make_prj_single(snapshot, filename, basepath)
 
 if __name__ == '__main__':
 
