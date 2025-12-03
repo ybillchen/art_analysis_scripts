3f153adf78e3b764502376c65f23c91cf74be64f
Bill Chen (Mac) <ybchen@umich.edu>
2025-12-03 09:27:54 -0500

    updated

--- art_analysis_scripts/track_tree.py
+++ art_analysis_scripts/track_tree.py
@@ -250,7 +250,8 @@ def star_at_last_snapshot(mpb, filename_list_for_tree, basepath):
     t_ave = ave_time(d)
     t_dur = duration(d)
     t_spread = age_spread(d)
-    out = np.column_stack([initial_mass, f_bound0, t_form, t_ave, t_dur, t_spread])
+    eps_int = get_eps_int(d)
+    out = np.column_stack([initial_mass, f_bound0, t_form, t_ave, t_dur, t_spread, eps_int])
 
     savename = filename.replace('out/snap_', 'analysis/star_at_').replace('.art', '.txt')
     np.savetxt(savename, out)
@@ -302,7 +303,7 @@ if __name__ == '__main__':
 
     filename_list_for_tree = snap_list['filename'][dsnap:]
 
-    # star_at_last_snapshot(mpb_main, filename_list_for_tree, basepath)
-    skirt_interface_at_last_snapshot(mpb_main, filename_list_for_tree, basepath)
+    star_at_last_snapshot(mpb_main, filename_list_for_tree, basepath)
+    # skirt_interface_at_last_snapshot(mpb_main, filename_list_for_tree, basepath)
 
     # make_prj_along_mpb(mpb_main, filename_list_for_tree, basepath)
