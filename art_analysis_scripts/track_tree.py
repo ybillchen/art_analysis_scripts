5fe19337dd4f1e6a3377a7960ce9fe2d3356c684
Bill Chen (Mac) <ybchen@umich.edu>
2026-04-21 11:06:14 -0400

    updated

--- art_analysis_scripts/track_tree.py
+++ art_analysis_scripts/track_tree.py
@@ -334,7 +334,7 @@ def process_folder(basepath, scalefactor=None):
         #     field="metallicity", field_unit="1", weight="mass", vmin=1e-5, vmax=1e-2
         # )
         make_prj_along_mpb(
-            mpb_main, filename_list_for_tree, basepath, scalefactor=scalefactor, cmap='PuOr',
+            mpb_main, filename_list_for_tree, basepath, scalefactor=scalefactor, cmap='RdYlGn',
             field="turb2ther", field_unit="1", weight="mass", vmin=1e-3, vmax=1e3, scale='log',
         )
 
