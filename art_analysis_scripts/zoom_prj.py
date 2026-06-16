66a9084bf0f70c3852713bfd148d57d3e4584ba2
Bill Chen (Mac) <ybchen@umich.edu>
2026-06-16 11:48:20 -0400

    updated

--- art_analysis_scripts/zoom_prj.py
+++ art_analysis_scripts/zoom_prj.py
@@ -242,14 +242,14 @@ if __name__ == '__main__':
             print("  Core %d levels in box: %s" % (core['rank'], np.unique(lev)))
             cell_y_kpc = cb[("index", "y")].to_value("kpc")
             cell_x_kpc = cb[("index", "x")].to_value("kpc")
-            level_colors = {15: 'cyan', 16: 'yellow', 17: 'orange', 18: 'red'}
-            for lvl, color in level_colors.items():
+            levels_present = sorted(np.unique(lev[lev < 13]))
+            cmap_lev = plt.cm.get_cmap('rainbow', max(len(levels_present), 1))
+            for i, lvl in enumerate(levels_present):
                 mask = lev == lvl
-                if mask.any():
-                    cell_dy = (cell_y_kpc[mask] - core['y_kpc']) * 1e3
-                    cell_dx = (cell_x_kpc[mask] - core['x_kpc']) * 1e3
-                    ax.scatter(cell_dy, cell_dx, s=4, color=color, alpha=0.8,
-                               ec='none', rasterized=True, label='L%d' % lvl)
+                cell_dy = (cell_y_kpc[mask] - core['y_kpc']) * 1e3
+                cell_dx = (cell_x_kpc[mask] - core['x_kpc']) * 1e3
+                ax.scatter(cell_dy, cell_dx, s=4, color=cmap_lev(i), alpha=0.8,
+                           ec='none', rasterized=True, label='L%d' % lvl)
 
             n_H = core['density'] * X_H / m_H_g
             ax.set_title(r'$n_{\rm H} = %.1e\ {\rm cm}^{-3}$' % n_H, fontsize=10)
