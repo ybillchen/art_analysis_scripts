7d05ef2d8128b5b1a21b2abb40281684afa6d8df
Bill Chen (Mac) <ybchen@umich.edu>
2026-06-22 11:51:01 -0400

    updated

--- art_analysis_scripts/zoom_prj.py
+++ art_analysis_scripts/zoom_prj.py
@@ -308,7 +308,7 @@ if __name__ == '__main__':
             cx_pc, cy_pc, cz_pc = core['x_kpc'] * 1e3, core['y_kpc'] * 1e3, core['z_kpc'] * 1e3
             r_pc = np.sqrt((cell_x_pc - cx_pc)**2 + (cell_y_pc - cy_pc)**2 + (cell_z_pc - cz_pc)**2)
             mass_cell = rho_gcc * dx_cm**3    # grams per cell
-            r_edges   = np.logspace(np.log10(0.1), np.log10(100.0), PROFILE_N_BINS + 1)
+            r_edges   = np.logspace(np.log10(0.1), np.log10(half_pc), PROFILE_N_BINS + 1)
             r_centers = np.sqrt(r_edges[:-1] * r_edges[1:])   # geometric mean
             pc_to_cm  = 3.0857e18
             r_edges_cm = r_edges * pc_to_cm
@@ -320,7 +320,7 @@ if __name__ == '__main__':
             ax1.plot(r_centers[valid], nH_prof[valid], lw=1.5, color='C0')
             ax1.set_xscale('log')
             ax1.set_yscale('log')
-            ax1.set_xlim(0.1, 100.0)
+            ax1.set_xlim(0.1, half_pc)
             ax1.set_xlabel('r (pc)', fontsize=9)
             if i == 0:
                 ax1.set_ylabel(r'$n_{\rm H}$ (cm$^{-3}$)', fontsize=9)
